d28fa958de6d28689574c984356e3518d4a1509e
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-09 02:11:25 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,24 @@ function calculateSalesTax(amount, taxRate){
 //  lines 23 and 24 run the calculateSalesTax function usin the test data
 calculateSalesTax(100, 0.07);
 calculateSalesTax(500, 0.1);
+
+
+// Task 3: Arrow Function
+
+
+// the lines below are an arrow function that runs an if statement and depending on what your performance rating is you get a different bonus amount
+const calculateBonus = (salary, performanceRating) => {
+    if (performanceRating === "Excellent") {
+        console.log(`Your Bonus is: $${salary * 0.2}`);
+    };
+    if (performanceRating === "Good") {
+        console.log(`Your Bonus is: $${salary * 0.1}`);
+    };
+    if (performanceRating === "Average") {
+        console.log(`Your Bonus is: $${salary * 0.05}`);
+    };
+};
+
+//  lines 44 and 45 run the function above using the test data
+calculateBonus(5000, "Excellent");
+calculateBonus(7000, "Good");
