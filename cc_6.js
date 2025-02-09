b82b3c129c0d9df99438302d7f5428041d92fd72
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-09 01:43:09 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,16 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 //  lines 9 and 10 run the calculatedProfit function using test data
 calculateProfit(20, 30, 100);
 calculateProfit(50, 70, 200);
+
+
+// Task 2: Function Expression
+
+
+// the lines below are a function that calculates the amount of sales tax and logs it to the console using template literals
+function calculateSalesTax(amount, taxRate){
+    console.log(`The sales tax is: $${Math.round(amount * taxRate)}`);
+};
+
+//  lines 23 and 24 run the calculateSalesTax function usin the test data
+calculateSalesTax(100, 0.07);
+calculateSalesTax(500, 0.1);
